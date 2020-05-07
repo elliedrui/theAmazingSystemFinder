9896c65e18ccadf9addd34da8c14973b7f9b851b
jaesonwatts <illender.the.great@gmail.com>
2020-05-06 23:55:35 -0600

    building api calls, hourly commits(late)

--- lib/api.rb
+++ lib/api.rb
@@ -6,3 +6,42 @@ require 'json'
 
 # needs to make the call to the edsm api based on the use input from the cli class
 # should have methods available for the options we choose to allow, again, will be limited for brevity
+
+# a few notes on specific calls:
+
+# url = "https://www.edsm.net/api-v1/sphere-systems\?systemName=Deciat\&radius=10\&showPrimaryStar=1"  
+# uri = URI.parse(url)      
+# uri.open
+# request_result = uri.open   
+# json_parse = JSON.parse(request_result)     
+
+# json_parse.select {|item| item["primaryStar"]["type"]=="M (Red dwarf) Star"}    //this would return an array of systems in a 10ly radius of deciat that have red dwarf stars
+
+class Api < Cli
+
+  attr_accessor :name, :search_location, :system_search_parameter, :max_search_radius, :search_radius
+  
+  
+
+  def initialize
+    @name=name
+  
+    @search_location=search_location    
+    @system_search_parameter=system_search_parameter
+    @max_search_radius=max_search_radius
+  end
+
+
+  def self.request_systems_from_edsm(search_center, search_radius)
+    url = "https://www.edsm.net/api-v1/sphere-systems\?systemName=#{search_center}\&radius=#{search_radius}"
+    uri = URI.parse(url)      
+    uri.open
+    request_result = uri.open   
+    json_parse = JSON.parse(request_result.string)   
+    #binding.pry 
+    puts json_parse
+  end
+
+
+
+end
--- lib/cli.rb
+++ lib/cli.rb
@@ -6,28 +6,58 @@
 
 class Cli
 
-  attr_accessor :name, :system, :search_location, :system_search_parameter
+  attr_accessor :name, :search_location, :system_search_parameter, :max_search_radius, :search_radius
 
   
 
   def initialize
     @name=name
-    @system=system
+    # @system=system
     @search_location=search_location    
     @system_search_parameter=system_search_parameter
+    @max_search_radius=max_search_radius
   end
 
 
   def self.GreetCommander
+    system "clear"
     puts "---------------------"
-    puts "Greeting Commander!"
+    puts "Greetings Commander!"
     puts "Please enter your search center:"
-    search_location = gets.chomp
+    $search_location = gets.chomp
     puts "---------------------"
+    puts "Please enter max search radius:"
+      max_search_radius = gets.to_i
+      if max_search_radius == 0
+        puts "We need an integer greater than zero, Commander"
+        max_search_radius = gets.to_i
+      end
+    #binding.pry
+    puts "---------------------"
+    self.scraperforchris
+    Api.request_systems_from_edsm(@search_location, @max_search_radius)
   end
 
-
-
-
+  def self.scraperforchris
+    system "clear"
+    20.times do
+      puts "scraping..........\\"
+      sleep(0.03)
+      system "clear"
+      puts "scraping..........\\"
+      sleep(0.03)
+      system "clear"
+      puts "scraping..........|"
+      sleep(0.03)
+      system "clear"
+      puts "scraping........../"
+      sleep(0.03)
+      system "clear"
+      puts "scraping..........-"
+      sleep(0.03)
+      system "clear"
+    end
+    
+  end
 
 end
